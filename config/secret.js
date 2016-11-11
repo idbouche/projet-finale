a183de6d04a29115156cb4089327bac0eaaa6360
Mohammed Id Bouche <idbouche2@gmail.com>
2016-11-11 20:07:04 +0100

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -3,7 +3,7 @@ module.exports = {
   database: 'mongodb://network:network@ds061506.mlab.com:61506/socialnetwork',
   port: 3000,
   secretKey: "Arash@$@!#@",
-  userEmail : "idbouche2@gmail.com",
-  pass : "AmirA2008"
+  userEmail : "***************",
+  pass : "*********"
 }
 // mongodb://<mongomomo>:<123456>@ds017776.mlab.com:17776/mongomomo
